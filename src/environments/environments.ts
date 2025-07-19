379c637a1fc25ee60197c70f92ef9d809064210d
davidhardin <thisgoestodavid@gmail.com>
2025-07-19 11:29:10 +0300

    fixed broken emitter

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiBaseUrl: 'http://localhost:3000',
-  //   apiBaseUrl: 'https://nestor-b7eb2f7c8808.herokuapp.com/comments/top',
+  // apiBaseUrl: 'http://localhost:3000',
+  apiBaseUrl: 'https://nestor-b7eb2f7c8808.herokuapp.com/comments/top',
 };
