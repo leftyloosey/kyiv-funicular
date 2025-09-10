eab1349bf65e5e90c05e0c2427e02fb2687fe888
davidhardin <thisgoestodavid@gmail.com>
2025-09-10 20:29:08 +0300

    trim again

--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -1,65 +1,64 @@
-import { inject, Injectable, OnInit, signal } from '@angular/core';
-import { User } from '../../utils/interfaces/user';
-import { environment } from '../../environments/environments';
-import { HttpClient } from '@angular/common/http';
-import { Token } from '../../utils/classes/token';
-import { map } from 'rxjs';
-import { CookieService } from 'ngx-cookie-service';
-// import { jwtDecode } from 'jwt-decode';
-import { userNameSignal } from '../../utils/signals';
-import { Router } from '@angular/router';
-import { createLinkedSignal } from '@angular/core/primitives/signals';
+// import { inject, Injectable, OnInit, signal } from '@angular/core';
+// import { User } from '../../utils/interfaces/user';
+// import { environment } from '../../environments/environments';
+// import { HttpClient } from '@angular/common/http';
+// import { Token } from '../../utils/classes/token';
+// import { map } from 'rxjs';
+// import { CookieService } from 'ngx-cookie-service';
+// import { userNameSignal } from '../../utils/signals';
+// import { Router } from '@angular/router';
+// import { createLinkedSignal } from '@angular/core/primitives/signals';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthService {
-  private token: Token = new Token('');
-  private router = inject(Router);
-  private http = inject(HttpClient);
-  private cookieService = inject(CookieService);
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class AuthService {
+//   private token: Token = new Token('');
+//   private router = inject(Router);
+//   private http = inject(HttpClient);
+//   private cookieService = inject(CookieService);
 
-  constructor() {
-    this.token = new Token(this.getTokenFromService());
-  }
+//   constructor() {
+//     this.token = new Token(this.getTokenFromService());
+//   }
 
-  private setUserNameSignal(name: string) {
-    userNameSignal.set(name);
-  }
+//   private setUserNameSignal(name: string) {
+//     userNameSignal.set(name);
+//   }
 
-  public getUserId(): string {
-    return this.token.getUserId();
-  }
+//   public getUserId(): string {
+//     return this.token.getUserId();
+//   }
 
-  public logout() {
-    this.cookieService.delete('token');
-    this.setUserNameSignal('');
+//   public logout() {
+//     this.cookieService.delete('token');
+//     this.setUserNameSignal('');
 
-    if (this.router.url == '/home') window.location.reload();
-    this.router.navigate(['/home']);
-    this.isAuthenticated();
-  }
+//     if (this.router.url == '/home') window.location.reload();
+//     this.router.navigate(['/home']);
+//     this.isAuthenticated();
+//   }
 
-  public getTokenFromBackend(name: string, password: string) {
-    let url = `${environment.apiBaseUrl}/auth/login`;
+//   public getTokenFromBackend(name: string, password: string) {
+//     let url = `${environment.apiBaseUrl}/auth/login`;
 
-    return this.http.post<User>(url, { name, password }).pipe(
-      map((response: any) => {
-        if (response.access_token) {
-          this.cookieService.set('token', response.access_token);
-          this.token = new Token(this.getTokenFromService());
-        }
-        return response;
-      })
-    );
-  }
+//     return this.http.post<User>(url, { name, password }).pipe(
+//       map((response: any) => {
+//         if (response.access_token) {
+//           this.cookieService.set('token', response.access_token);
+//           this.token = new Token(this.getTokenFromService());
+//         }
+//         return response;
+//       })
+//     );
+//   }
 
-  public isAuthenticated(): boolean {
-    return this.cookieService.check('token') && !this.token.isExpired();
-  }
+//   public isAuthenticated(): boolean {
+//     return this.cookieService.check('token') && !this.token.isExpired();
+//   }
 
-  private getTokenFromService(): string {
-    const value = this.cookieService.get('token');
-    return value;
-  }
-}
+//   private getTokenFromService(): string {
+//     const value = this.cookieService.get('token');
+//     return value;
+//   }
+// }
--- src/app/utils/signals.ts
+++ src/app/utils/signals.ts
@@ -1,8 +1,6 @@
 import { signal } from '@angular/core';
-import { Attempt } from './classes/attempt';
 
 export const userNameSignal = signal<string>('');
-export const commentSignal = signal<Attempt>({ entry: '', parentId: '' });
 export const indexSignal = signal<number>(0);
 
 export const inputSafetyStorage = signal<string>('');
